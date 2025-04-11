7d2651d60e7f9a97c1cd9c2352c551b0bf563fa0
rickie <rcya996001@gmail.com>
2025-04-11 10:33:22 -0400

    updated theme, with redesign

--- app/src/main/java/com/mapd721/group2/MainActivity.kt
+++ app/src/main/java/com/mapd721/group2/MainActivity.kt
@@ -15,7 +15,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.mapd721.group2.ui.theme.Group2Theme
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
